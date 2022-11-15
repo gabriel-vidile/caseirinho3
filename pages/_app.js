6210aece0fd20aaa4df955698ea0afed56cc9426
Gabriel Vidile <gabriel.vidile@trinks.com>
2022-11-15 00:16:41 -0300

    appjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast';
+import { Footer } from '../components/Footer';
+import Navbar from '../components/navbar';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <ChakraProvider>
+        <Toaster />
+        <Navbar />
+        <Component {...pageProps} />;
+        <Footer />
+      </ChakraProvider>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
